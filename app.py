c53a3deb88afd2929d5bc55e1080ff608388282c
vv-grinko <65661924+vv-grinko@users.noreply.github.com>
2021-01-30 21:03:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,33 @@ from flask import make_response
 app = Flask(__name__)
 
 # создали ендпоинт
-@app.route('/webhook2')
+# @app.route('/webhook2')
+# def hello_slack():
+#     # получили данные из запроса
+#     request_json = request.get_json(silent=True, force=True)
+#     # тут ваш код возьмет запрос и вернет в ответ любой dict объект ответа, можно даже пустой
+#     # примерно так request_json -> response_body_json
+#     ...
+#     response_body = json.dumps(request_json)
+#     # упаковали все в корректный респонс
+#     response = make_response((response_body['challenge']),200)
+#     response.headers['Content-Type'] = 'text/plain'
+#     # и вернули
+#     return response
+
+# if __name__ == '__main__':
+#     port = int(os.getenv('PORT', 5000))
+#     app.run(debug=False, port=port, host='0.0.0.0')
 def hello_slack():
     # получили данные из запроса
     request_json = request.get_json(silent=True, force=True)
     # тут ваш код возьмет запрос и вернет в ответ любой dict объект ответа, можно даже пустой
     # примерно так request_json -> response_body_json
     ...
-    response_body = json.dumps(request_json)
+    response_body = json.dumps(response_body_json)
     # упаковали все в корректный респонс
-    response = make_response((response_body['challenge']),200)
-    response.headers['Content-Type'] = 'text/plain'
+    response = make_response(response_body)
+    response.headers['Content-Type'] = 'application/json'
     # и вернули
     return response
 
