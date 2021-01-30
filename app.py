9e2b15b15a0aa80f2b7a7aec9c6525d08e8ffce4
vv-grinko <65661924+vv-grinko@users.noreply.github.com>
2021-01-30 23:06:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def message(payload):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 3000))
-    app.run(debug=False, port=port, host='0.0.0.0')
+#     port = int(os.getenv('PORT', 3000))
+    app.run(debug=True)
 
 # создали ендпоинт
 # @app.route('/webhook2')
