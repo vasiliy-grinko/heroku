a919d03e4a92e8f73a763d66e37d2016de5728a6
vv-grinko <65661924+vv-grinko@users.noreply.github.com>
2021-01-31 12:05:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,81 @@
-import os
-from slack_bolt import App
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-#  Initializes your app with your bot token and signing secret
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-)
-
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
-# import slack
 # import os
+# from slack_bolt import App
 # from pathlib import Path
 # from dotenv import load_dotenv
-# from flask import Flask, request, Response
-# from slackeventsapi import SlackEventAdapter
-# import string
+
 # env_path = Path('.') / '.env'
 # load_dotenv(dotenv_path=env_path)
+# #  Initializes your app with your bot token and signing secret
+# app = App(
+#     token=os.environ.get("SLACK_BOT_TOKEN"),
+#     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+# )
 
-# app = Flask(__name__)
-# slack_event_adapter = SlackEventAdapter(
-#        os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
+# # Start your app
+# if __name__ == "__main__":
+#     app.start(port=int(os.environ.get("PORT", 3000)))
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+import string
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-# client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-# # BOT_ID = client.api_call("auth.test")['user_id']
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+       os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
 
-
-# BAD_WORDS = ['hmm', 'no', 'tim']
-
-# def check_if_bad_words(message):
-#     msg = message.lower()
-#     msg = msg.translate(str.maketrans('', '', string.punctuation))
-
-#     return any(word in msg for word in BAD_WORDS)
-
-
-# @ slack_event_adapter.on('message')
-# def message(payload):
-#     event = payload.get('event', {})
-#     channel_id = event.get('channel')
-#     user_id = event.get('user')
-#     text = event.get('text')
-
-#     if user_id != None and BOT_ID != user_id:
-#         if user_id in message_counts:
-#             message_counts[user_id] += 1
-#         else:
-#             message_counts[user_id] = 1
-
-#         if text.lower() == 'start':
-#             send_welcome_message(f'@{user_id}', user_id)
-#         elif check_if_bad_words(text):
-#             ts = event.get('ts')
-#             client.chat_postMessage(
-#                 channel=channel_id, thread_ts=ts, text="THAT IS A BAD WORD!")
+client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+# BOT_ID = client.api_call("auth.test")['user_id']
 
 
-# @ app.route('/message-count', methods=['POST'])
-# def message_count():
-#     data = request.form
-#     user_id = data.get('user_id')
-#     channel_id = data.get('channel_id')
-#     message_count = message_counts.get(user_id, 0)
+BAD_WORDS = ['hmm', 'no', 'tim']
 
-#     client.chat_postMessage(
-#         channel=channel_id, text=f"Message: {message_count}")
-#     return Response(), 200
+def check_if_bad_words(message):
+    msg = message.lower()
+    msg = msg.translate(str.maketrans('', '', string.punctuation))
+
+    return any(word in msg for word in BAD_WORDS)
 
 
+@ slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    if user_id != None and BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+
+        if text.lower() == 'start':
+            send_welcome_message(f'@{user_id}', user_id)
+        elif check_if_bad_words(text):
+            ts = event.get('ts')
+            client.chat_postMessage(
+                channel=channel_id, thread_ts=ts, text="THAT IS A BAD WORD!")
+
+
+@ app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+
+    client.chat_postMessage(
+        channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 3000))
+    app.run(debug=False, port=port)
 # if __name__ == "__main__":
 #        app.start(port=int(os.environ.get("PORT", 3000)))
 #     port = int(os.getenv('PORT', 3000))
